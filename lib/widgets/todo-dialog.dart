fd88ab2e5ac92dc56102ca754cac6956468af214
Paichana <paichana5@gmail.com>
2019-07-25 23:35:34 +0700

    Remove unused import

--- lib/widgets/todo-dialog.dart
+++ lib/widgets/todo-dialog.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
 import 'package:datetime_picker_formfield/datetime_picker_formfield.dart';
-import 'package:provider/provider.dart';
 import 'package:simple_todo/models/task.dart';
 
 import '../providers/tasks.dart';
