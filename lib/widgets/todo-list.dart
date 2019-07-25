2ab04c348013d178b6afb9599420fcb89f0a350f
Paichana <paichana5@gmail.com>
2019-07-25 23:20:39 +0700

    Add icon when dismiss

--- lib/widgets/todo-list.dart
+++ lib/widgets/todo-list.dart
@@ -44,21 +44,12 @@ class _TodoListState extends State<TodoList> {
                   taskData.removeTaskIndex(i);
                 },
                 background: Container(
-                  decoration: BoxDecoration(
-                    // Box decoration takes a gradient
-                    gradient: LinearGradient(
-                      // Where the linear gradient begins and ends
-                      begin: Alignment.topRight,
-                      end: Alignment.bottomLeft,
-                      // Add one stop for each color. Stops should increase from 0 to 1
-                      stops: [0.1, 0.5, 0.7, 0.9],
-                      colors: [
-                        // Colors are easy thanks to Flutter's Colors class.
-                        Colors.red[900],
-                        Colors.red[600],
-                        Colors.red[300],
-                        Colors.red[0],
-                      ],
+                  color: Theme.of(context).primaryColor,
+                  child: Center(
+                    child: Icon(
+                      Icons.delete,
+                      size: 70,
+                      color: Theme.of(context).accentColor,
                     ),
                   ),
                 ),
