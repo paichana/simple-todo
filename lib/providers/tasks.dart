025d2f814d3e0cf013af35831daad4c79aecb417
Paichana <paichana5@gmail.com>
2019-07-29 20:15:00 +0700

    fix bug when getting data from shared_preference

--- lib/providers/tasks.dart
+++ lib/providers/tasks.dart
@@ -6,8 +6,6 @@ import 'dart:convert';
 import '../models/task.dart';
 
 class TasksProvider with ChangeNotifier {
-  List<Task> _tasks = [];
-  List<Task> _done = [];
   Map<String, TodoList> td = Map();
 
   DateFormat keyFormatter = DateFormat("yMd");
@@ -22,14 +20,12 @@ class TasksProvider with ChangeNotifier {
     String key = keyFormatter.format(dt);
 
     if (td.containsKey(key)) {
-      _tasks = td[key].tasks;
-      _done = td[key].done;
       return;
     }
 
     TodoList temptd = TodoList();
     await SharedPreferences.getInstance().then((prefs) {
-      List<String> pdone = prefs.getStringList('done_${key}') ?? [];
+      List<String> pdone = prefs.getStringList('done_$key') ?? [];
 
       if (pdone.length > 0) {
         temptd.done = pdone.map((t) {
@@ -40,7 +36,7 @@ class TasksProvider with ChangeNotifier {
         temptd.done = [];
       }
 
-      List<String> ptask = prefs.getStringList("tasks") ?? [];
+      List<String> ptask = prefs.getStringList("tasks_$key") ?? [];
 
       if (ptask.length > 0) {
         temptd.tasks = ptask.map((t) {
@@ -71,8 +67,14 @@ class TasksProvider with ChangeNotifier {
     if (td.containsKey(key)) {
       return td[key].tasks;
     } else {
-      td[key] = TodoList();
-      return td[key].tasks;
+      loadTask(dt).then((t) {
+        if (td.containsKey(key)) {
+          notifyListeners();
+        } else {
+          td[key] = TodoList();
+        }
+      });
+      return [];
     }
   }
 
@@ -81,8 +83,14 @@ class TasksProvider with ChangeNotifier {
     if (td.containsKey(key)) {
       return td[key].done;
     } else {
-      td[key] = TodoList();
-      return td[key].done;
+      loadTask(dt).then((t) {
+        if (td.containsKey(key)) {
+          notifyListeners();
+        } else {
+          td[key] = TodoList();
+        }
+      });
+      return [];
     }
   }
 
@@ -118,6 +126,7 @@ class TasksProvider with ChangeNotifier {
 
     if (!td.containsKey(key)) {
       td[key] = TodoList();
+      td[key].tasks.add(t);
     }
     td[key].tasks.add(t);
     notifyListeners();
@@ -141,10 +150,10 @@ class TasksProvider with ChangeNotifier {
       return;
     }
 
-    if (i > _tasks.length - 1) {
-      td[dt].done.removeAt(i - _tasks.length);
+    if (i > td[key].tasks.length - 1) {
+      td[key].done.removeAt(i - td[key].tasks.length);
     } else {
-      td[dt].tasks.removeAt(i);
+      td[key].tasks.removeAt(i);
     }
 
     notifyListeners();
